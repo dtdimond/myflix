c796a43810efd1b83b85508aa345961708bdd0f7
Dan Dimond <dtdimond@gmail.com>
2015-09-30 19:21:06 -0400

    Fixed error in Video.search_by_title where we weren't protecting against sql injection.

--- app/models/video.rb
+++ app/models/video.rb
@@ -7,7 +7,7 @@ class Video < ActiveRecord::Base
   def self.search_by_title(search_term)
     return [] if search_term.blank?
 
-    Video.where("title LIKE '%" + search_term + "%'").order(:title)
+    Video.where("title LIKE ?", "%#{search_term}%").order(:title)
   end
 
 end
