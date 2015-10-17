cb0c402ae21f1da0814cfa1fd4140508fb1fadf5
Dan Dimond <dtdimond@gmail.com>
2015-10-17 09:22:42 -0400

    Refactored controller tests to take advantage of rspec macros and shared examples.

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -2,63 +2,49 @@ require 'spec_helper'
 
 describe VideosController do
   describe 'GET show' do
-    context 'with authentication' do
-      let!(:video) { Fabricate(:video) }
+    let!(:video) { Fabricate(:video) }
+    before { set_current_user }
 
-      before do
-        session[:user_id] = Fabricate(:user).id
-        get :show, id: video.id
-      end
+    it 'sets the @video' do
+      get :show, id: video.id
+      expect(assigns(:video)).to eq(video)
+    end
 
-      it 'sets the @video' do
-        expect(assigns(:video)).to eq(video)
-      end
+    it 'sets the @reviews with most recent first' do
+      get :show, id: video.id
+      expect(assigns(:reviews)).to eq(video.reviews.sort_by { |r| r.created_at }.reverse!)
+    end
 
-      it 'sets the @reviews with most recent first' do
-        expect(assigns(:reviews)).to eq(video.reviews.sort_by { |r| r.created_at }.reverse!)
-      end
+    it 'renders the show template' do
+      get :show, id: video.id
+      expect(response).to render_template(:show)
+    end
 
-      it 'renders the show template' do
-        expect(response).to render_template(:show)
-      end
-   end
-
-    context 'without authentication' do
-      it 'redirects to login_path' do
-        get :show, id: 1
-        expect(response).to redirect_to(login_path)
-      end
+    it_behaves_like "requires sign in" do
+      let(:action) { get :show, id: 1 }
     end
   end
 
   describe 'GET search' do
-    context 'with authentication' do
-      before { session[:user_id] = Fabricate(:user).id }
+    before { set_current_user }
 
-      it 'sets @videos' do
-        video = Fabricate(:video, title: "Finding Nemo")
-        video2 = Fabricate(:video, title: "Nemosis")
-        video3 = Fabricate(:video, title: "Finding Dory")
+    it 'sets @videos' do
+      video = Fabricate(:video, title: "Finding Nemo")
+      video2 = Fabricate(:video, title: "Nemosis")
+      video3 = Fabricate(:video, title: "Finding Dory")
 
-        get :search, search_term: "Nem"
-        expect(assigns(:videos)).to eq([video, video2])
-      end
-
-      it 'renders the search template' do
-        video = Fabricate(:video, title: "Finding Nemo")
-
-        get :search, search_term: "Nem"
-        expect(response).to render_template(:search)
-      end
+      get :search, search_term: "Nem"
+      expect(assigns(:videos)).to eq([video, video2])
     end
 
-    context 'without authentication' do
-      it 'redirects to login_path' do
-        get :search, search_term: "Nem"
-        expect(response).to redirect_to(login_path)
-      end
+    it 'renders the search template' do
+      video = Fabricate(:video, title: "Finding Nemo")
+      get :search, search_term: "Nem"
+      expect(response).to render_template(:search)
+    end
+
+    it_behaves_like "requires sign in" do
+      let(:action) { get :search, search_term: "Nem" }
     end
   end
 end
-
-
